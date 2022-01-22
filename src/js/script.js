0e3896752aea46cb667a9ef33d13758415601f06
Mantas9707 <86239940+Mantas9707@users.noreply.github.com>
2022-01-22 18:46:54 +0200

    naujas

---
+++ src/js/script.js
@@ -0,0 +1,91 @@
+"use strict";
+
+const dataInfo = [{
+    link: "../img/img-1.png",
+    name: "Sad Clown.png",
+    size: "3 MB"
+    },
+    {
+      link: "../img/img-2.png",
+      name: "Waiting for the train.png",
+      size: "1.5 MB"  
+    },
+
+    {
+        link:"../img/img-3.png",
+        name: "Little friends.jpg",
+        size: "1.5 MB"
+    },
+
+    {
+        link:"../img/img-4.png",
+        name: "What A Wonderful.jpg",
+        size: "4.5 MB"
+    },
+
+    {
+        link:"../img-4.png",
+        name: "Gymnast.jpg",
+        size: "2 MB"
+    },
+
+    {
+        link:"../img/img-raw-1.png",
+        name: "Blue Dandelion Drops.jpg",
+        size: "3 MB"
+    },
+
+    {
+        link:"../img/img-raw-2.png",
+        name: "Poppy for Mom.png",
+        size: "1.5 MB"
+    },
+
+    {
+        link:"../img/img-raw-3.png",
+        name: "Drop some Droplets.png",
+        size: "1.5 MB"
+    },
+
+    {
+        link:"../img/img-raw-4.png",
+        name: "Daisy.png",
+        size: "4.5 MB"
+    },
+
+    {
+        link:"../img/img-raw-5.png",
+        name: "Sunshine.png",
+        size: "2 MB"
+    },
+
+    {
+        link:"../img/img-raw2-1.png",
+        name: "Blue Dandelion Drops.jpg",
+        size: "3 MB"
+    },
+
+    {
+        link:"../img/img-raw2-2.png",
+        name: "Poppy for Mom.png",
+        size: "1.5 MB"
+    },
+
+    {
+        link:"../img/img-raw2-3.png",
+        name: "Drop some Droplets.png",
+        size: "1.5 MB"
+    },
+
+    {
+        link:"../img/img-raw2-4.png",
+        name: "Daisy.png",
+        size: "4.5 MB"
+    },
+
+    {
+        link:"../img/img-raw2-5.png",
+        name: "Sunshine.png",
+        size: "2 MB"
+    },
+]
