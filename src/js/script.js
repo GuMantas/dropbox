04a33739bd25d01b456e9d7dd341384b263075c3
Mantas9707 <86239940+Mantas9707@users.noreply.github.com>
2022-01-27 10:32:08 +0200

    change

--- src/js/script.js
+++ src/js/script.js
@@ -1,112 +1,54 @@
-"use strict";
-
-
-
-const dataInfo = [
-    {
-    link: "/img/img-1.png",
-    name: "Sad Clown.png",
-    size: 3
-    },
-    {
-      link: "/img/img-2.png",
-      name: "Waiting for the train.png",
-      size: 1.5  
-    },
-
-    {
-        link:"/img/img-3.png",
-        name: "Little friends.jpg",
-        size: 1.5
-    },
-
-    {
-        link:"/img/img-4.png",
-        name: "What A Wonderful.jpg",
-        size: 4.5
-    },
-
-    {
-        link:"/img/img-5.png",
-        name: "Gymnast.jpg",
-        size: 2
-    },
-
-    {
-        link:"/img/img-raw-1.png",
-        name: "Blue Dandelion Drops.jpg",
-        size: 3
-    },
-
-    {
-        link:"/img/img-raw-2.png",
-        name: "Poppy for Mom.png",
-        size: 1.5
-    },
-
-    {
-        link:"/img/img-raw-3.png",
-        name: "Drop some Droplets.png",
-        size: 1.5
-    },
-
-    {
-        link:"/img/img-raw-4.png",
-        name: "Daisy.png",
-        size: 4.5
-    },
-
-    {
-        link:"/img/img-raw-5.png",
-        name: "Sunshine.png",
-        size: 2
-    },
-
-    {
-        link:"/img/img-raw2-1.png",
-        name: "Blue Dandelion Drops.jpg",
-        size: 3
-    },
-
-    {
-        link:"/img/img-raw2-2.png",
-        name: "Poppy for Mom.png",
-        size: 1.5
-    },
-
-    {
-        link:"/img/img-raw2-3.png",
-        name: "Drop some Droplets.png",
-        size: 1.5
-    },
-
-    {
-        link:"/img/img-raw2-4.png",
-        name: "Daisy.png",
-        size: 4.5
-    },
-
-    {
-        link:"/img/img-raw2-5.png",
-        name: "Sunshine.png",
-        size: 2
-    },
-];
+    "use strict";
 
 
 
 
- function getImage (){
-    let containerImage = document.querySelector(".image")
+
+    const images = [];
+
+    function getImage (arr){
+    let containerImage = document.getElementById("image")
       let content = "";
   
-        for (let i = 0; i < dataInfo.length; i++){
-        content += `<img src='${dataInfo[i].link}' alt='' />
-        <p>${dataInfo[i].name}</p>
-       <p>${dataInfo[i].size}MB</p>`;
-        containerImage.innerHTML = content;
-          console.log(content);
+        for (let i = 0; i < arr.length; i++){
+        content += `<article class='images'>
+        <div class='img col-lg-3 col-md-6' style='background-image: url(${arr[i].path})'></div>
+        <p>${arr[i].name}</p>
+       <p>${convertBytes(arr[i].size)} MB</p></article>`;
+
         }
+        containerImage.innerHTML = content;
+        getCapacity(arr);
     }
     
-getImage();
+    document.getElementById("uploadBtn").addEventListener("click", function() {
+       
+        for(let i = 0; i < document.getElementById("file").files.length; i++) {
+            const element = document.getElementById("file").files[i];
+            images.push({
+                name: element.name,
+                size: element.size,
+                path: URL.createObjectURL(element),
+            });
+            
+        }
+        
+        getImage(images);
+        
+    });
+
+
+function convertBytes(bytes) {
+    return (bytes / 1048576).toFixed(2);
+}
+
+
+function getCapacity(arr){
+    let capacityBytes = 0;
+    for(let i of arr){
+        capacityBytes += i.size;
+    }
+const capacityMb = convertBytes(capacityBytes);
+document.getElementById("capacityValue").textContent = `${capacityMb} MB / 100 MB`; 
+document.getElementById("capacity").value = capacityMb
+}
